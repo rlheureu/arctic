a2ff4cf2509e44961b57a2da78914be5f6e965cb
shanr <shanrandhawa@calspl.com>
2016-08-15 11:41:10 -0700

    Forgot exceptions

---
+++ utils/exception.py
@@ -0,0 +1,12 @@
+'''
+Created on Aug 14, 2016
+
+@author: shanrandhawa
+'''
+
+
+class InvalidInput(Exception): pass
+
+class ClaimExistsException(Exception): pass
+
+class ClaimInvalidException(Exception): pass
