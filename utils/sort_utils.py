494e116cf303b11773f194045dec77e797037775
shanr <shanrandhawa@calspl.com>
2017-06-27 15:15:26 -0700

    Fixed sort order for non recommended parts

--- utils/sort_utils.py
+++ utils/sort_utils.py
@@ -8,8 +8,8 @@ Created on May 31, 2017
 def get_score_available_and_recommended(part):
     if hasattr(part, "compatible") and not part.compatible: return -1
     if part.recommended and part.available: return 2 + int(part.sort_order) if part.sort_order else 2
-    elif part.available and len(part.prices) > 0: return 1
-    else: return 0 
+    elif part.available and len(part.prices) > 0: return 1  + int(part.sort_order) if part.sort_order else 1
+    else: return int(part.sort_order) if part.sort_order else 0 
 
 def sort_by_available_and_recommended(a,b):
     """
