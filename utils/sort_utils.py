dcb54a367d36ea78af15dda0335554b2a304f053
shanr <shanrandhawa@calspl.com>
2017-06-27 13:39:02 -0700

    Fixed manual sort order

--- utils/sort_utils.py
+++ utils/sort_utils.py
@@ -7,7 +7,7 @@ Created on May 31, 2017
 
 def get_score_available_and_recommended(part):
     if hasattr(part, "compatible") and not part.compatible: return -1
-    if part.recommended and part.available: return 2 + part.sort_order if part.sort_order else 2
+    if part.recommended and part.available: return 2 + int(part.sort_order) if part.sort_order else 2
     elif part.available and len(part.prices) > 0: return 1
     else: return 0 
 
